bcbf8c82578ecff0fecf037c08fc56205c6ff3a5
usp-evgeniya <usp-evgeniya@yandex.ru>
2017-08-17 15:32:17 +0300

    Accordeons wo animation, internal links, js simplification

--- js/main.js
+++ js/main.js
@@ -3,58 +3,32 @@ $(document).ready(function () {
 
     /* hamburger-menu */
     $(function(){
-            var hamburgerMenu = $('.hamburger-menu');
+        var hamburgerMenu = $('.hamburger-menu');
         hamburgerMenu.on('click', function (e) {
             e.preventDefault();
             hamburgerMenu.toggleClass('open');
-
-            if (hamburgerMenu.hasClass('open')) {
-                
-                $('.header__nav').slideDown(700);
-                hamburgerMenu.css({
-                    background: "url('img/icon/close.svg') 50% 50% no-repeat",
-                    opacity: '0.7'
-                });
-                $('body').css({
-                    height: '100vh',
-                    overflow: 'hidden'
-                });
-            } else {
-                $('.header__nav').slideUp(700);
-                hamburgerMenu.css({
-                    background: "url('img/icon/hamburger-menu.svg') 50% 50% no-repeat",
-                    opacity: '1'
-                });
-                $('body').css({
-                    height: '100%',
-                    overflow: 'initial'
-                });
-
-            }
+            $('body').toggleClass('open');
+            $('.header__nav').slideToggle(700);
         })
     })
-    
-
 
     /* team-accordeon */
+    /*Как теперь сделать, чтобы оно анимировалось - плавно открывалось и закрывалось?*/
+    /*Я специально оставила возможный вариант, который не сработал, кроме того slideToggle убивает flex*/
 
     $(function() {
         $('.team__member').on('click', function (e) {
             e.preventDefault();
             var elem = $(e.target),
-            active = elem.closest('.team__item'),
-            result = elem.next('.team__details');
+            active = elem.closest('.team__item');
+            /*result = elem.next('.team__details');*/
 
-            if (active.hasClass('active')) {
-                active.removeClass('active');
-                result.css('height','0rem')
-                
-            } else {
-                active.addClass('active');
-                result.css('height','6.625rem');
-                active.siblings().find('.team__details').css('height','0rem');
-                active.siblings().removeClass('active')
-            }
+                active.toggleClass('active');
+                /*result.slideToggle(500);*/
+                active.siblings().removeClass('active');
+                /*active.siblings().find('.team__details').slideUp(500);
+                */
+            
         })
 
     })
@@ -63,42 +37,40 @@ $(document).ready(function () {
 
     $(function() {
         $('.menu__type-title').on('click', function (e) {
-            console.log($(e.target))
-            
+          
             e.preventDefault();
             var elem = $(e.target),
-            active = elem.closest('.menu__item'),
-            result = active.find('.menu__description');
+            active = elem.closest('.menu__item');
 
-            if (active.hasClass('active')) {
-                active.removeClass('active');
-                result.css({
-                    'width':'0rem',
-                    'padding': '0rem'
-                })
-                
-            } else {
-                active.addClass('active');
-                result.css({
-                    'width':'37rem',
-                    'padding': '3.75rem 2.1875rem'
-                })
-                active.siblings().find('.menu__description').css({
-                    'width':'0rem',
-                    'padding': '0rem'
-                });
-                active.siblings().removeClass('active')
-            }
-
-            
+            active.toggleClass('active');
+            active.siblings().removeClass('active');
         })
-
     })
 
+    /*bullets*/
 
+    $(function(){
+            
 
+        $('.bullets__link').on('click', function (e) {
+       /* e.preventDefault();    */
+        var elem = $(e.target),
+        /*target = elem.attr('href'),*/
+        marker = elem.closest('.bullets__item');
+        
+        marker.toggleClass('bullets__item--active');
+        marker.siblings().removeClass('bullets__item--active');
+
+        console.log(target);
+        console.log(marker);            
+
+        /*
+        $('html, body').animate({
+            scrollTop : $('target').offset().top
+                
+        }, 2000);
+*/
+        })
+    })
 
 })
-
-
-
